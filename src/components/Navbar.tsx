7c1fe75eabffcf1b4c011130a20a3e2c99f01d67
rubenbupe <rubenbupe@gmail.com>
2025-07-15 14:54:07 +0200

    fix: added legal disclaimer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,23 @@
-import logoAdif from '../assets/logo-adif.webp';
+import { TrainTrackIcon } from 'lucide-react';
+// import logoAdif from '../assets/logo-adif.webp';
 import ThemeSelect from './theme-select';
 
 export default function Navbar() {
 	return (
 		<nav className="flex items-center p-4 bg-sidebar border-b border-foreground/10 sticky top-0 z-1 gap-4">
-			<div
+			{/* <div
 				className="md:max-w-[150px] md:w-[150px] md:h-[40px] max-w-[75px] w-[75px] h-[40px]"
 				style={{
 					backgroundColor: 'var(--sidebar-foreground)',
 					WebkitMask: `url(${logoAdif}) center/contain no-repeat`,
 					mask: `url(${logoAdif}) center/contain no-repeat`
 				}}
-				aria-label="ADIF Logo"
-			/>
-			<h1 className="md:text-4xl text-xl text-sidebar-foreground font-light">
-				<span className="font-extrabold">Configurar</span> Panel
+				aria-label="Logo"
+			/> */}
+			<TrainTrackIcon className="text-sidebar-foreground h-8 md:h-12 w-8 md:w-12" />
+			<h1 className="md:text-4xl text-xl text-sidebar-foreground font-light relative">
+				<span className="font-extrabold">Configurar</span> <span>Ferropanel</span>
+				<small className="absolute italic text-sm -bottom-4 -right-6">A**f compliant</small>
 			</h1>
 			<div className="ml-auto">
 				<ThemeSelect />
