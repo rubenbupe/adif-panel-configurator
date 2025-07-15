cd72303b3030682abcc7d160a00c742869497e81
rubenbupe <rubenbupe@gmail.com>
2025-07-15 14:12:02 +0200

    fix: added legal disclaimer

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -65,7 +65,11 @@ export default function Configurator() {
 	return (
 		<div className="w-screen min-h-screen max-w-screen flex flex-col">
 			<Navbar />
-			<div className="w-full max-w-full flex-1 flex flex-col items-center justify-center bg-background gap-4">
+			<div className="w-full max-w-full flex-1 flex flex-col items-center justify-center bg-background gap-4 mt-4">
+				<p className="max-w-2xl text-sidebar-foreground text-center text-balance bg-sidebar p-4 rounded-lg mx-4">
+					Esta herramienta <strong>NO ES OFICIAL</strong> y <strong>NO ESTÁ AFILIADA</strong> a Adif de ninguna manera.
+					Está diseñada para ayudar a los usuarios a consultar los paneles informaticos de las estaciones de Adif.
+				</p>
 				<div className="grid lg:grid-cols-2 items-center gap-4 w-full p-4 max-w-7xl">
 					<ConfiguratorForm
 						headerOptions={headerOptions}
@@ -82,7 +86,8 @@ export default function Configurator() {
 					<Preview url={url} />
 				</div>
 				<p className="max-w-2xl text-xs text-foreground/60 text-center">
-					Este proyecto no está afiliado a Adif. Algunas configuraciones pueden no funcionar como se espera.
+					Este proyecto no está afiliado a Adif y tiene proposito educacional. Algunas configuraciones pueden no
+					funcionar como se espera.
 				</p>
 				<div className="flex items-center justify-center">
 					<GithubIcon className="h-3.5 w-3.5 inline-block mr-1 stroke-foreground/60" />
